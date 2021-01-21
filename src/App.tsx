621af2b669f4561dfc6255553d67ef646508391b
Perseus Jenri Magbutay <perseus.magbutay@thomsonreuters.com>
2021-01-21 08:36:35 +0800

    updated app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button'
 import './App.css';
 import ButtonAppBar from './components/app_bar/ButtonAppBar';
+import ImageCard from './components/image_card/ImageCard';
 
 class App extends Component {
   render() {
     return (
-      <ButtonAppBar></ButtonAppBar>
-    );
+      <div className="app">
+        <ButtonAppBar></ButtonAppBar>
+        <ImageCard></ImageCard>
+      </div>
+    )
   }
 }
 
